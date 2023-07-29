08ad8103bd40e03af4704ccf7c167797e7779ee7
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-07-29 05:30:26 +0000

    Atividade Joyclass

--- AtividadesEmJavaScript/002Calculator.js
+++ AtividadesEmJavaScript/002Calculator.js
@@ -3,36 +3,41 @@ const readline = require('readline-sync');
 
 //função título
 function titulo(){
-    console.log("------------------------------\n     C A L C U L A D O R A    \n------------------------------\n");
+    console.log("\n------------------------------\n     C A L C U L A D O R A    \n------------------------------\n");
 }
 
 //função menu
 function menu(){
-    console.log('1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\nR= ');
+    console.log('1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\n0. Sair\n');
 }
 
 //função calculadora
 function calc(n1,n2,op){
-    var res;
+    let res;
+    let msg = "  ";
     switch(op){
         case 1:
-            res = n1+n2;
+            res = n1+n2; 
+            console.log("\n",n1,'+',n2,'=',res,);
             break;
         case 2:
             res = n1-n2;
+            console.log("\n",n1,'-',n2,'=',res);
             break;
         case 3:
             res = n1*n2;
+            console.log("\n",n1,'*',n2,'=',res);
             break;
         case 4:
             res = n1/n2;
+            console.log("\n",n1,'/',n2,'=',res);
             break;
     }
+    return msg;
 }
 
 //declaração da variável operador, menu, num1 e num2
 let operador;
-let me;
 let num1;
 let num2;
 
@@ -41,18 +46,18 @@ do{
     titulo();
     menu();
 
-    me = readline.questionInt("\nSelecione uma opção: ");
+    operador = readline.questionInt("Selecione uma opção: ");
 
-    if(me == 0){
+    if(operador == 0){
         console.log("\nSaindo da calculadora...");
-    }else if(me != 1 || me != 2 || me != 3 || me != 4 || me != 0){
+        break;
+    }else if(operador > 4 || operador < 0){
         console.log("\nEssa opção não existe!");
     }else{
-        operador = me;
         num1 = readline.questionFloat("\nDigite o primeiro número: ");
-        num2 = readline.questionFloat("\nDigite o segundo número: ");
+        num2 = readline.questionFloat("Digite o segundo número: ");
 
-        console.log(calc(num1,num2,operador));
+        calc(num1,num2,operador);
     }
 
-}while (me != 0);
+}while (operador != 0);
