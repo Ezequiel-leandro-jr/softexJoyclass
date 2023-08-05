1b630e1bc0ee521b203d98bdf98e6f8df11d3df0
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-08-05 19:40:04 +0000

    update commit

--- AtividadesEmJavaScript/005Listas.js
+++ AtividadesEmJavaScript/005Listas.js
@@ -13,9 +13,9 @@ Ao terminar, imprima novamente todas as informações das três listas.
 const readline = require('readline-sync');
 
 //criação das listas
-let nomes = new Array(10);
-let idades = new Array(10);
-let cores = new Array(10);
+let nomes = new Array(3);
+let idades = new Array(3);
+let cores = new Array(3);
 let continuar = 1;
 let op = 0;
 
@@ -28,13 +28,14 @@ for(let i = 0; i < nomes.length; i++){
 }
 
 //impressão das listas
-console.log(`PESSOAS: ${nomes[0]}  ${nomes[1]}  ${nomes[2]}  ${nomes[3]}  ${nomes[4]}  ${nomes[5]}  ${nomes[6]}  ${nomes[7]}  ${nomes[8]}  ${nomes[9]}`);
-console.log(`IDADE:   ${idades[0]}           ${idades[1]}           ${idades[2]}           ${idades[3]}           ${idades[4]}           ${idades[5]}           ${idades[6]}           ${idades[7]}           ${idades[8]}           ${idades[9]}`);
-console.log(`CORES: ${cores[0]}         ${cores[1]}       ${cores[2]}         ${cores[3]}         ${cores[4]}         ${cores[5]}         ${cores[6]}         ${cores[7]}         ${cores[8]}         ${cores[9]}`);
+console.log("    \n");
+     console.log('PESSOAS: ', nomes.join(" | "));
+     console.log('IDADE:   ', idades.join(" | "));
+     console.log('CORES:   ', cores.join(" | "));
 
 //alterando as listas
 do{
-    console.log('MENU:\n\n1.Adicionar Pessoa\n2.Excluir Pessoa\n3.Alterar Pessoa\n');
+    console.log('\n\nMENU:\n1.Adicionar Pessoa\n2.Excluir Pessoa\n3.Alterar Pessoa\n');
     op = readline.questionInt("R= ");
 
     switch(op){
@@ -47,27 +48,101 @@ do{
         case 3:
             altPessoa();
             break;
-        Default:
+        default:
             console.log('Erro: opção inválida!');
             break;
     }
 
      console.log("    \n\n");
-     console.log(`PESSOAS: ${nomes[0]}  ${nomes[1]}  ${nomes[2]}  ${nomes[3]}  ${nomes[4]}  ${nomes[5]}  ${nomes[6]}  ${nomes[7]}  ${nomes[8]}  ${nomes[9]}`);
-     console.log(`IDADE:   ${idades[0]}           ${idades[1]}           ${idades[2]}           ${idades[3]}           ${idades[4]}           ${idades[5]}           ${idades[6]}           ${idades[7]}           ${idades[8]}           ${idades[9]}`);
-     console.log(`CORES: ${cores[0]}         ${cores[1]}       ${cores[2]}         ${cores[3]}         ${cores[4]}         ${cores[5]}         ${cores[6]}         ${cores[7]}         ${cores[8]}         ${cores[9]}`);
+     console.log('PESSOAS: ', nomes.join(" | "));
+     console.log('IDADE:   ', idades.join(" | "));
+     console.log('CORES:   ', cores.join(" | "));
 
 
-    continuar = questionInt('Deseja voltar ao MENU? <1-sim 0-não> ');
+    continuar = readline.questionInt('\nDeseja voltar ao MENU? <1-sim 0-não> ');
 }while(continuar != 0)
 
 //funções
 function addPessoa(){
     let n, i, c;
-    n = readline.question('Digite o nome da pessoa: ');
+    n = readline.question('\nDigite o nome da pessoa: ');
     nomes.push(n);
     i = readline.questionInt(`Digite a idade de ${n}: `);
     idades.push(i);
-    c = readline.questionInt(`Digite a cor favorita de ${n}: `);
+    c = readline.question(`Digite a cor favorita de ${n}: `);
     cores.push(c);
+
+    console.log("\nPessoa adicionada!");
 }
+
+function excPessoa(){
+    let n;
+    console.log("   \n")
+    for(let i = 0; i < nomes.length; i++){
+        console.log(`${i}.`, nomes[i]);
+    }
+
+    do{
+        n = readline.questionInt('\nDigite o n° da pessoa a ser excluída: ');
+
+        if(n >= nomes.length){
+            console.log('\nErro: opção inválida.');
+        }
+    }while(n >= nomes.length)
+    
+    nomes.splice(n, 1);
+    idades.splice(n, 1);
+    cores.splice(n, 1);
+
+    console.log('\nPessoa removida!');
+    
+}
+
+function altPessoa(){
+
+    let p, op;
+
+    console.log("   \n");
+
+    for(let i = 0; i < nomes.length; i++){
+        console.log(`${i}.`, nomes[i]);
+    }
+
+    console.log("   \n");
+
+    do{
+        p = readline.questionInt('Digite o n° da pessoa a ser alterada: ');
+
+        if(p >= nomes.length){
+            console.log('\nErro: opção inválida.');
+        }
+    }while(p >= nomes.length)
+
+    do{
+        console.log('\nO que você deseja alterar:\n1.Nome\n2.Idade\n3.Cor favorita\n4.Todas as opções\n');
+        op = readline.questionInt('R= ');
+
+        switch(op){
+            case 1:
+                nomes[p] = readline.question('\nDigite o nome: ');
+                break;
+            case 2:
+                idades[p] = readline.questionInt('Digite a idade: ');
+                break;
+            case 3:
+                cores[p] = readline.question('Digite a cor favorita: ');
+                break;
+            case 4:
+                nomes[p] = readline.question('\nDigite o nome: ');
+                idades[p] = readline.questionInt('Digite a idade: ');
+                cores[p] = readline.question('Digite a cor favorita: ');
+                break;
+            default:
+                console.log('\nErro: opção inválida.');
+                break;
+        }
+    }while(op != 1 && op != 2 && op != 3 && op != 4)
+
+    console.log('\nPessoa alterada.')
+}
+
